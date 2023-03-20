65c0c6ab6ecce1f9d3a9c722619f4e7fbeca7f23
M Hazwan <37287035+hazxone@users.noreply.github.com>
2023-03-20 18:20:04 +0800

    Create main.rs
    Main function to test the lib

---
+++ main.rs
@@ -0,0 +1,19 @@
+fn main() {
+    if is_minimum("foobar", 4)
+    {
+      println!("Yay");
+    }
+    else
+    {
+      println!("Nay");
+    }
+
+    if char_in_string("mee@mail.com", '@')
+    {
+      println!("Yay");
+    }
+    else
+    {
+      println!("Nay");
+    }
+}
