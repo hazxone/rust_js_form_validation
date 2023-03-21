8ed2411f901abaa58663fc5d7c2faea4a191f068
M Hazwan <37287035+hazxone@users.noreply.github.com>
2023-03-21 09:41:31 +0800

    Update main.rs
    Update email val test

--- main.rs
+++ main.rs
@@ -16,4 +16,8 @@ fn main() {
     {
       println!("Nay");
     }
+    
+    let email = "example@example.com";
+    let is_valid = is_valid_email(email);
+    println!("Is {} a valid email? {}", email, is_valid);
 }
